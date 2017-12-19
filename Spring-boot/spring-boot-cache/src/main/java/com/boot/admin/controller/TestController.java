5c153e9d694839f66861b8b0c22cdb0236d46494
tuonioooo <daizhaoman@sina.com>
2017-12-19 16:23:03 +0800

    git
    Spring boot 结合cache的配置使用

---
+++ Spring-boot/spring-boot-cache/src/main/java/com/boot/admin/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.boot.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.admin.service.UserService;
+
+@RestController  
+@RequestMapping(value = "/admin/v1")  
+public class TestController {  
+    
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "/first", method = RequestMethod.GET)
+	@ResponseBody
+    public String firstResp (Long id){
+		return userService.query(id);
+    }
+	
+	@RequestMapping(value = "/put", method = RequestMethod.GET)
+	@ResponseBody
+    public String put(Long id, String value){
+		return userService.put(id, value);
+    }
+	
+	@RequestMapping(value = "/put2", method = RequestMethod.GET)
+	@ResponseBody
+    public String put2(Long id, String value){
+		return userService.put2(id, value);
+    }
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	@ResponseBody
+    public void delete(Long id){
+		userService.remove(id);
+    }
+} 
